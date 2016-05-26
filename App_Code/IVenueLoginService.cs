42edb5628583a5c77d25cbcd42e1f640e5d204a9
Marcus Joyner <marcus@seattlemarcus.com>
2016-05-26 11:53:47 -0700

    Initial Commit

---
+++ App_Code/IVenueLoginService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IVenueLoginService" in both code and config file together.
+[ServiceContract]
+public interface IVenueLoginService
+{
+    [OperationContract]
+    bool RegisterVenue( VenueInfo v);
+
+    [OperationContract]
+
+    int venueLogin(string username, string password);
+}
+
+public class VenueInfo
+{
+    [DataMember]
+    public string VenueName { set; get; }
+
+    [DataMember]
+    public string VenueAddress { set; get; }
+
+    [DataMember]
+    public string VenueCity { set; get; }
+
+    [DataMember]
+    public string VenueState { set; get; }
+
+    [DataMember]
+    public string VenueZipCode { set; get; }
+
+    [DataMember]
+    public string VenuePhone { set; get; }
+
+    [DataMember]
+    public string VenueEmail { set; get; }
+
+    [DataMember]
+    public string VenueWebPage { set; get; }
+
+    [DataMember]
+    public int VenueAgeRestriction { set; get; }
+
+    [DataMember]
+    public string VenueLoginUserName { set; get; }
+
+    [DataMember]
+    public string VenueLoginPasswordPlain { set; get; }
+}
